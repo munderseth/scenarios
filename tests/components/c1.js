134b1122e59d2279d4b6343587e5160feb65a522
Mark Underseth <marku@s2technologies.com>
2026-07-20 13:14:21 -0700

    change inputs

--- tests/components/c1.js
+++ tests/components/c1.js
@@ -1,10 +1,6 @@
-async function c1(page, { p1, p2, m1, x1, c1, c2 } = {}) {
+async function c1(page, { p1} = {}) {
     p1 = "one";
-    p2 = "two";
-    m1 = "m1";
-    x1 = "x1"
-    c1 = "c1";
-    c2 = "c2";
+  
     await page.click('#user-name');
 }
 
