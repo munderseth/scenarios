7c7945a428cf766f6a4fd83c075c710b9c5013d3
Mark Underseth <marku@s2technologies.com>
2026-07-20 13:11:48 -0700

    inputs for c1

--- tests/components/c1.js
+++ tests/components/c1.js
@@ -1,8 +1,10 @@
-async function c1(page, { p1, p2, m1, x1, c1 } = {}) {
+async function c1(page, { p1, p2, m1, x1, c1, c2 } = {}) {
     p1 = "one";
     p2 = "two";
     m1 = "m1";
     x1 = "x1"
+    c1 = "c1";
+    c2 = "c2";
     await page.click('#user-name');
 }
 
