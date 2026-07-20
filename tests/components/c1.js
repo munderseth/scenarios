5b0979092367df2228d69f58cd7acfad581aa6cb
Mark Underseth <marku@s2technologies.com>
2026-07-20 08:25:07 -0700

    change c1

--- tests/components/c1.js
+++ tests/components/c1.js
@@ -1,7 +1,8 @@
-async function c1(page, { p1, p2, m1 } = {}) {
+async function c1(page, { p1, p2, m1, x1 } = {}) {
     p1 = "one";
     p2 = "two";
     m1 = "m1";
+    x1 = "x1"
     await page.click('#user-name');
 }
 
