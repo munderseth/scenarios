e4586bd0fa66813b6a6338a00d8271a8db689af7
Mark Underseth <marku@s2technologies.com>
2026-07-22 15:42:40 -0700

    s1

---
+++ tests/scenarios/s1.spec.js
@@ -0,0 +1,23 @@
+// ===== STARTING POINT: tests/scenarios/s1.spec.js =====
+/**
+ * Scenario: s1
+ * Description: now lets see if this works
+ *
+ * Cases:
+ *  - c1 case (3 steps)
+ *    1. Navigate to https://example.com
+ *    2. Navigate to https://example.com
+ *    3. Navigate to https://example.com
+ */
+const { test, expect } = require('@playwright/test');
+
+test.describe("s1", () => {
+  test.describe.configure({ mode: 'serial' });
+  let v1; // scenario variable
+
+  test('c1', async ({ page }) => {
+    // Step 1: Navigate to https://example.com
+    // Step 2: Navigate to https://example.com
+    // Step 3: Navigate to https://example.com
+  });
+});
