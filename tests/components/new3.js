c0bb6841a7f440aac386e85279779032081d2ac2
munderseth <marku@s2technologies.com>
2026-07-28 08:05:15 -0700

    new3

---
+++ tests/components/new3.js
@@ -0,0 +1,15 @@
+
+/**
+ * Component: new3
+ * Description: lets make changes. Stuff and more, now will work
+ *
+ * Steps:
+ *   1. Navigate to https://example.com
+ */
+async function new3(page, { p1} = {}) {
+  if (p1 == null) throw new Error("Required input 'p1' is not set");
+
+  // Step 1: Navigate to https://example.com
+}
+
+module.exports = { new3};
